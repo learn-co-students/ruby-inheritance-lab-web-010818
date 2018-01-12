4c8872cbf67ee0427eb96e3c6271f1188da46e2f
Anna Yang <yanganna68@gmail.com>
2018-01-12 09:46:18 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative 'user.rb'
+require_relative 'teacher.rb'
+
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(arr)
+    @knowledge << arr
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
